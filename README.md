52fea428789e1db8ad99dd9bea632385eb27e51f
Ayush Kaithwas <ayush.91011@gmail.com>
2024-03-26 22:01:32 +0530

    init

---
+++ README.md
@@ -0,0 +1,49 @@
+# Assam University Alumni Website Server
+
+This is the server that handles the requests made from Assam University's Alumni Website's client.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+## MakeFile
+
+run all make commands with clean tests
+```bash
+make all build
+```
+
+build the application
+```bash
+make build
+```
+
+run the application
+```bash
+make run
+```
+
+Create DB container
+```bash
+make docker-run
+```
+
+Shutdown DB container
+```bash
+make docker-down
+```
+
+live reload the application
+```bash
+make watch
+```
+
+run the test suite
+```bash
+make test
+```
+
+clean up binary from the last build
+```bash
+make clean
+```
---
+++ internal/server/routes.go
@@ -0,0 +1,47 @@
+package server
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+func (s *Server) RegisterRoutes() http.Handler {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+	r.Get("/", s.HelloWorldHandler)
+	r.Get("/health", s.healthHandler)
+
+	return r
+}
+
+func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	resp := make(map[string]string)
+	resp["message"] = "Hello World"
+
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		fmt.Printf("error handling JSON marshal. Err: %v", err)
+	}
+
+	_, _ = w.Write(jsonResp)
+}
+
+func (s *Server) healthHandler(w http.ResponseWriter, r *http.Request) {
+	jsonResp, _ := json.Marshal(s.db.Health())
+	_, _ = w.Write(jsonResp)
+}
